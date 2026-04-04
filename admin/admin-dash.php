49d88270af879c035a4d170f81dee31eaa0cb202
tannersloan1 <tannersloan05@gmail.com>
2026-04-03 19:58:45 -0500

    dashboard buttons for redirects

--- admin/admin-dash.php
+++ admin/admin-dash.php
@@ -8,6 +8,15 @@ include "../includes/activity.php";
 if ($_SESSION["role"] != "admin") {
     die("Restricted access"); 
 }
+
+$sql = $conn->prepare("SELECT email, full_name FROM ADMIN_INFO WHERE admin_id=?");
+$sql->bind_param("i", $_SESSION["user_id"]);
+if ($sql->execute()) {
+    $admin = $sql->get_result()->fetch_assoc();
+}
+else {
+    $name = "NO NAME IN DATABASE.";
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,5 +29,37 @@ if ($_SESSION["role"] != "admin") {
     </head>
     <body>
         <?php include "../includes/header.php"; ?>
+
+        <?php if ($admin): ?>
+            <h1 class="dash-hello">Hello, <?php echo htmlspecialchars($admin["full_name"]); ?>!</h1>
+        <?php else: ?>
+            <h1 class="dash-hello">Hello, <?php echo htmlspecialchars($name); ?>!</h1>
+        <?php endif; ?>
+
+        <div class="dash-container">
+            <div class="dash-grid">
+                <a href="student.php">
+                    <div class="card">
+                        <div class="card-content">
+                            <p>Student</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="teacher.php">
+                    <div class="card">
+                        <div class="card-content">
+                            <p>Teacher</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="analytics.php">
+                    <div class="card">
+                        <div class="card-content">
+                            <p>Analytics</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
     </body>
 </html>
