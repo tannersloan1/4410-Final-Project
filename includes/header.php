36ab19210d9f2254996ce7d952a08a1bc9b0f411
tannersloan1 <tannersloan05@gmail.com>
2026-04-10 14:52:09 -0500

    added account info

--- includes/header.php
+++ includes/header.php
@@ -10,20 +10,27 @@ $role = $_SESSION["role"] ?? null;
     <div class="nav-inner">
         <div class="header">LMS System</div>
         <div class="links">
-        <a href="/lms/index.php">Home</a>  
-        <?php if ($isLoggedIn): ?>
-            <?php if ($role === "student"): ?>
-            <a href="/lms/student/student-dash.php">Dashboard</a>
-            <?php elseif ($role === "teacher"): ?>
-            <a href="/lms/teacher/teacher-dash.php">Dashboard</a>
+            <a href="/lms/index.php">Home</a>  
+            <?php if ($isLoggedIn): ?>
+                <?php if ($role === "student"): ?>
+                    <a href="/lms/student/student-dash.php">Dashboard</a>
+                <?php elseif ($role === "teacher"): ?>
+                    <a href="/lms/teacher/teacher-dash.php">Dashboard</a>
+                <?php else: ?>
+                    <a href="/lms/admin/admin-dash.php">Dashboard</a>
+                <?php endif; ?>
+                <a href="/lms/logout.php">Logout</a>
+                <?php if ($role === "student"): ?>
+                    <a href="/lms/student/student-info.php">Account Info</a>
+                <?php elseif ($role === "teacher"): ?>
+                    <a href="/lms/teacher/teacher-info.php">Account Info</a>
+                <?php else: ?>
+                    <a href="/lms/admin/admin-info.php">Account Info</a>
+                <?php endif; ?>
             <?php else: ?>
-            <a href="/lms/admin/admin-dash.php">Dashboard</a>
+                <a href="/lms/login.php">Login</a>
+                <a href="/lms/register.php">Create Account</a>
             <?php endif; ?>
-            <a href="/lms/logout.php">Logout</a>
-        <?php else: ?>
-            <a href="/lms/login.php">Login</a>
-            <a href="/lms/register.php">Create Account</a>
-        <?php endif; ?>
         </div>
     </div>
 </div>
